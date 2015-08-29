659ac885d5e6041077c5f843d68b15c0658e6551
hegdeashwin <ashwin.hegde3@gmail.com>
2015-08-29 20:18:43 +0530

    Defines initial possible states with url

--- client/app/nodemyadmin.js
+++ client/app/nodemyadmin.js
@@ -8,16 +8,50 @@
 	]);
 
 	app.config(function($stateProvider, $urlRouterProvider) {
-		$urlRouterProvider.otherwise('/pagenotfound')
+		$urlRouterProvider.otherwise('/pagenotfound');
 
 		$stateProvider
-			.state('login', {
-
+			.state('signin', {
+				url: '/sin'
 			})
-			.state('dashboard', {
-
+			.state('signout', {
+				url: '/sout'
 			})
-			.state('')
+			.state('dboard', {
+				url: '/dboard'
+			})
+			.state('dbs', {
+				url: '/dbs'
+			})
+			.state('db', {
+				url: '/dbs/:dbName'
+			})
+			.state('tbls', {
+				url: '/tbls'
+			})
+			.state('tbl', {
+				url: '/tbls/:tblName'
+			})
+			.state('dsql', {
+				url: '/dsql'
+			})
+			.state('ipt', {
+				url: '/ipt'
+			})
+			.state('users', {
+				url: '/users'
+			})
+			.state('user', {
+				url: '/users/:uname'
+			});
+	});
 
+	app.run(function($rootScope, $state) {
+		$rootScope.$on('$stateChangeError', function(event, toState, toParams, fromState, fromParams, error) {
+			event.preventDefault();
+			if (error === 'AUTH_REQUIRED') {
+				$state.go('signin');
+			}
+		});
 	});
 })();
