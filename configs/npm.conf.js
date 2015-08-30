b0254b47e11995b4cf3bb5d87fb608c2b7d12bb6
hegdeashwin <ashwin.hegde3@gmail.com>
2015-08-31 00:34:32 +0530

    Re-factors npm-conf comments

--- configs/npm.conf.js
+++ configs/npm.conf.js
@@ -1,22 +1,20 @@
+/**
+ * NPM's configuration setup.
+ *
+ * How to use:
+ * var <npm-name> = require('../../configs/npm.conf').<npm-exported-name>;
+ */
+
 (function() {
 	'use strict';
 
 	/**
-	 * NPM's configuration setup.
-	 *
-	 * Support you want to use [nodegit] in some another file, Do the following
-	 *
-	 * Include it using following Syntax:
-	 * var nodegit = require('../../config/npm.config').nodegit;
-	 *
-	 */
-
-	/**
 	 * Joi: Object schema description language and validator for JavaScript objects.
-	 *
 	 * More info: https://github.com/hapijs/joi
+	 *
+	 * var joi = require('../../configs/npm.conf').joi;
 	 */
 	var joi = require('joi');
 	exports.joi = joi;
-	
+
 })();
