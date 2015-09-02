70bfc67217dad92e531598ca635d589ef4124137
hegdeashwin <ashwin.hegde3@gmail.com>
2015-09-03 02:27:19 +0530

    Initial signin controller + dashboard files

--- client/app/nodemyadmin.js
+++ client/app/nodemyadmin.js
@@ -7,51 +7,24 @@
 		require('angular-ui-router')
 	]);
 
+	app.controller('SigninCtrl', require('./signin/signin-controller'));
+
 	app.config(function($stateProvider, $urlRouterProvider) {
 		$urlRouterProvider.otherwise('/pagenotfound');
 
 		$stateProvider
 			.state('signin', {
-				url: '/sin'
-			})
-			.state('signout', {
-				url: '/sout'
+				url: '/sin',
+				templateUrl: './app/signin/signin-tpl.html',
+				controller: 'SigninCtrl',
+				controllerAs: 'signin'
 			})
 			.state('dboard', {
-				url: '/dboard'
-			})
-			.state('dbs', {
-				url: '/dbs'
-			})
-			.state('db', {
-				url: '/dbs/:dbName'
-			})
-			.state('tbls', {
-				url: '/tbls'
-			})
-			.state('tbl', {
-				url: '/tbls/:tblName'
-			})
-			.state('dsql', {
-				url: '/dsql'
-			})
-			.state('ipt', {
-				url: '/ipt'
-			})
-			.state('users', {
-				url: '/users'
-			})
-			.state('user', {
-				url: '/users/:uname'
+				url: '/dboard',
+				templateUrl: './app/dashboard/dashboard-tpl.html',
+				controller: 'DashboardCtrl',
+				controllerAs: 'dboard'
 			});
 	});
 
-	app.run(function($rootScope, $state) {
-		$rootScope.$on('$stateChangeError', function(event, toState, toParams, fromState, fromParams, error) {
-			event.preventDefault();
-			if (error === 'AUTH_REQUIRED') {
-				$state.go('signin');
-			}
-		});
-	});
 })();
