bb1dc39facec3309a9cdb80a3efff11869993f64
hegdeashwin <ashwin.hegde3@gmail.com>
2015-12-07 00:50:21 +0530

    Adds database + user list via datatables

--- static-site/scripts.js
+++ static-site/scripts.js
@@ -2,9 +2,39 @@
   'use strict';
 
   $(document).ready(function() {
-    $(".chosen-select").chosen({disable_search_threshold: 10});
+    $(".chosen-select").chosen({
+      disable_search_threshold: 10,
+      no_results_text: "Oops, nothing found!",
+      width: "100%" // https://github.com/harvesthq/chosen/issues/795
+    });
 
-    $('#databases').DataTable();
+    $('#databasesList').DataTable({
+      "pagingType": "full_numbers",
+      "order": [],
+      "columnDefs": [ {
+        "targets"  : [0, 3],
+        "orderable": false
+      }],
+      "dom": '<"dttoolbar">frtip'
+    });
+
+    $("div.dttoolbar").html('<button disabled="disabled" class="btn btn-sm btn-danger">Delete All</button>').css({
+      "float": "left"
+    });
+
+    $('#usersList').DataTable({
+      "pagingType": "full_numbers",
+      "order": [],
+      "columnDefs": [ {
+        "targets"  : [0, 6],
+        "orderable": false
+      }],
+      "dom": '<"dttoolbar">frtip'
+    });
+
+    $("div.dttoolbar").html('<button disabled="disabled" class="btn btn-sm btn-danger">Delete All</button>').css({
+      "float": "left"
+    });
   });
 
 })();
