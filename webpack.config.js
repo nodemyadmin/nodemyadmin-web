3d5e82ea3b54a3aa35986d562022606bf6845e9d
hegdeashwin <ashwin.hegde3@gmail.com>
2016-04-24 16:38:44 +0530

    Adds initial webpack config

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var config = {
+  entry: './src/nodemyadmin.js',
+
+  output: {
+    path: './dist/',
+    filename: 'nodemyadmin.min.js'
+  },
+
+  plugins: [new HtmlWebpackPlugin({
+    template: './src/index.html'
+  })],
+
+  module: {
+    loaders: [{
+      test: /\.(jpg|jpeg|png|gif|svg)$/i,
+      loader: 'file'
+    }, {
+      test: /\.less$/,
+      loader: 'style-loader!css-loader!less-loader'
+    }, {
+      test: /\.css$/,
+      loader: 'style-loader!css-loader'
+    }, {
+      test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+      loader: "url-loader?limit=10000&mimetype=application/font-woff"
+    }, {
+      test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+      loader: "file"
+    }]
+  }
+};
+
+module.exports = config;
