54d371e08bbdbcd626a8256c884b69f69b6ad310
hegdeashwin <ashwin.hegde3@gmail.com>
2016-05-15 15:40:08 +0530

    Add angular-loading-bar

--- src/nodemyadmin.js
+++ src/nodemyadmin.js
@@ -3,11 +3,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/css/bootstrap-theme.min.css';
 import 'font-awesome/css/font-awesome.min.css';
+import 'angular-loading-bar/build/loading-bar.min.css';
 
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
+import loadingBar from 'angular-loading-bar';
 
-let nodemyadmin = angular.module('nodemyadmin', [uiRouter]);
+let nodemyadmin = angular.module('nodemyadmin', [uiRouter, loadingBar]);
+
+nodemyadmin.config(['cfpLoadingBarProvider'], function(cfpLoadingBarProvider) {
+  cfpLoadingBarProvider.includeSpinner = false;
+});
 
 nodemyadmin.config(function($stateProvider, $urlRouterProvider) {
 
