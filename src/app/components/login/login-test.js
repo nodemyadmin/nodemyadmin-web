7047c3efa302aa7c33f4878216d9fc6264188c6d
hegdeashwin <ashwin.hegde3@gmail.com>
2016-07-11 01:36:50 +0530

    Adds real test case on Dashboard

--- src/app/components/login/login-test.js
+++ src/app/components/login/login-test.js
@@ -1,19 +1,7 @@
 'use strict';
 
-import DashboardCtrl from './dashboard-ctrl';
-
 describe('Login controller', function () {
     it('should have a Login controller', function () {
-        expect(1 + 2).toEqual(3);
-    });
-});
-
-describe('User login form', function () {
-    it('Ensure invalid email are caught', function () {
-        expect(1 + 2).toEqual(3);
-    });
-
-    it('Ensure valid email pass validation', function () {
         expect(1 + 2).toEqual(3);
     });
 });
