f2b07999b755d1f9855cceec2915a2b646f5909d
hegdeashwin <ashwin.hegde3@gmail.com>
2016-07-14 03:04:59 +0530

    Fixes build fail

--- src/app/pages/dashboard/dashboard.js
+++ src/app/pages/dashboard/dashboard.js
@@ -5,9 +5,8 @@ import uiRouter from 'angular-ui-router';
 
 import DashboardRoute from './dashboard-route';
 import DashboardCtrl from './dashboard-ctrl';
-import LoginDirective from '../../components/login/login-directive';
 
-export default angular.module('nodemyadmin.dashboard', [uiRouter, LoginDirective])
+export default angular.module('nodemyadmin.dashboard', [uiRouter])
   .config(['$stateProvider', DashboardRoute])
   .controller('DashboardCtrl', DashboardCtrl)
   .name;
