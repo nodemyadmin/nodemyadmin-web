149d691c458719b0915ac7cce44a19891eda93fe
hegdeashwin <ashwin.hegde3@gmail.com>
2016-07-19 02:02:34 +0530

    Re-factors navbar (Adds placement)

--- src/app/components/navbar/navbar-service.js
+++ src/app/components/navbar/navbar-service.js
@@ -6,18 +6,23 @@ class NavbarService {
   constructor() {
     this.navbar = {
       "navItems": [{
+        "title": "Dashboard",
         "label": "Dashboard",
         "linkTo": "#"
       }, {
+        "title": "Databases",
         "label": "Databases",
         "linkTo": "#databases"
       }, {
+        "title": "SQL",
         "label": "SQL",
         "linkTo": "#sql",
         "icon": "#"
       }, {
-        "label": "Help",
+        "label": "Developers Connection",
+        "title": "Developers Connection",
         "linkTo": "#",
+        "placement": "pull-right",
         "subItems": [{
           "label": "Github Home Page",
           "target": "_blank",
@@ -29,8 +34,51 @@ class NavbarService {
           "target": "_blank",
           "linkTo": "//github.com/hegdeashwin/nodemyadmin/releases"
         }, {
-          "label": "Release Page",
-          "linkTo": "#databases"
+          "label": "Milestones",
+          "target": "_blank",
+          "linkTo": "//github.com/hegdeashwin/nodemyadmin/milestones"
+        }, {
+          "divider": true
+        }, {
+          "label": "Issues Page",
+          "target": "_blank",
+          "linkTo": "//github.com/hegdeashwin/nodemyadmin/issues"
+        }, {
+          "label": "Open Pull Requests",
+          "target": "_blank",
+          "linkTo": "//github.com/hegdeashwin/nodemyadmin/pulls"
+        }, {
+          "divider": true
+        }, {
+          "label": "About nodemyadmin",
+          "target": "_blank",
+          "linkTo": "//github.com/hegdeashwin/nodemyadmin"
+        }]
+      }, {
+        "label": "Help",
+        "title": "More Information, Help etc.",
+        "linkTo": "#",
+        "placement": "pull-right",
+        "subItems": [{
+          "title": "Take a Tour",
+          "label": "Take a Tour",
+          "linkTo": "#"
+        }, {
+          "title": "What's New",
+          "label": "What's New",
+          "linkTo": "#"
+        }, {
+          "divider": true
+        }, {
+          "title": "API Explorer",
+          "label": "API Explorer",
+          "target": "_blank",
+          "linkTo": "#"
+        }, {
+          "title": "Documentation",
+          "label": "Documentation",
+          "target": "_blank",
+          "linkTo": "#"
         }]
       }]
     };
