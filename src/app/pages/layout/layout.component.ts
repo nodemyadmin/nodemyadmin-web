1c89c81e4ea60db5e830814139f275d5a56b4c7f
hegdeashwin <ashwin.hegde3@gmail.com>
2016-12-05 01:20:34 +0530

    Add header and footer component

--- src/app/pages/layout/layout.component.ts
+++ src/app/pages/layout/layout.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+
 import { HeaderComponent } from "../../components/header/header.component";
 import { FooterComponent } from "../../components/footer/footer.component";
 
